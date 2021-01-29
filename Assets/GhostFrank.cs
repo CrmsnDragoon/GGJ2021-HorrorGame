a450aeb0ad9deb107ba4a7382d12128ec0b90326
bec <rebecca.anne.kerr@gmail.com>
2021-01-29 18:13:21 +0800

    Starting Frank implementation

---
+++ Assets/GhostFrank.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostFrank : MonoBehaviour
+{
+    public Text healthText;
+    public Text keyText;
+    public GameObject growlCaption;
+    public GameObject FranksBros;
+    public AudioSource growl;
+
+
+    private static int healthCount = Global.health;
+    private static int keyCount = Global.key;
+
+
+    void Start()
+    {
+        GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SetCountText();
+            StartCoroutine(frankCallsBrothers());
+        }
+    }
+
+    IEnumerator frankCallsBrothers()
+    {
+        growl.Play();
+        Global.health--;
+        growlCaption.SetActive(true);
+        FranksBros.SetActive(true);
+        yield return new WaitForSeconds(1);
+        growlCaption.SetActive(false);
+    }
+}
