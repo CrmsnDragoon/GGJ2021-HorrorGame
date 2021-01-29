e3e3cadb15256c405c4efa035c9d6f2c5ae2c02b
Dylan Sinnott <crmsndragoon@gmail.com>
2021-01-29 16:34:38 +0800

    SubtitleController

---
+++ Assets/Scripts/main/SubtitleController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubtitleController : MonoBehaviour {
+    private static SubtitleController _instance;
+
+    public static SubtitleController Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+    }
+
+    [SerializeField] private Text SubtitleText;
+
+    public void ShowSubtitle(string subtitleText)
+    {
+        
+    }
+}
