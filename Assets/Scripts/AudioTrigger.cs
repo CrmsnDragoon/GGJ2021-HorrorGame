12dba05993d732202efb82a5621a12d85faf84bb
bec <rebecca.anne.kerr@gmail.com>
2021-01-29 19:23:35 +0800

    Nurse Lily is Done

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioSource soundTrigger;
+
+    private bool soundPlayed;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (soundPlayed == true)
+        {
+            return;
+        }
+        soundTrigger.Play();
+        soundPlayed = true;
+
+    }
+}
