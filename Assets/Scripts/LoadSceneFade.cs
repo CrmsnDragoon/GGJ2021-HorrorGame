2ff488f26b0f78a0ed375ce3cafeaf62d7232ec1
bec <rebecca.anne.kerr@gmail.com>
2021-01-30 13:24:24 +0800

    Trying again to show updates for menus

--- Assets/Scripts/LoadSceneFade.cs
+++ Assets/Scripts/LoadSceneFade.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LoadSceneOne : MonoBehaviour
+public class LoadSceneFade : MonoBehaviour
 {
     public Image fadeblack;
     public Animator fadeanim;
