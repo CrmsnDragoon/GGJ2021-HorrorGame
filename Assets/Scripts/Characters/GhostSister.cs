df6691df80886ad7ac31430e6d33e1784bf5ce62
bec <rebecca.anne.kerr@gmail.com>
2021-01-30 10:14:26 +0800

    A push to try and fix Sister

---
+++ Assets/Scripts/Characters/GhostSister.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostSister : MonoBehaviour
+{
+    public AudioSource goAway;
+    public GameObject sister1;
+
+    void Start()
+    {
+        GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            goAway.Play();
+            Global.Health--;
+            sister1.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
