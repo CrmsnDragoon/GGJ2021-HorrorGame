bcf907c22a128618694c16e3ac4d1af6bf2569ea
Dylan Sinnott <crmsndragoon@gmail.com>
2021-01-30 21:06:06 +0800

    Flatten Maggie, move the item list assignment to Emery so we don't need to have it in resources anymore.

--- Assets/Scripts/Characters/EnemyFollowTwo.cs
+++ Assets/Scripts/Characters/EnemyFollowTwo.cs
@@ -5,42 +5,47 @@ public class EnemyFollowTwo : MonoBehaviour
     public float speedSlow;
     public float speedFast;
     public AudioSource giggle;
-
-    public ItemListScriptableObject itemList;
     
+    public float soundLength;
+    
+    private float soundTimer = -1;
     private Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        //For WebGL, we may not have access to the Resources Folder
-        if(itemList != null) 
-        {
-            Global.SetItemList(itemList);
-        }
+        soundLength = giggle.clip.length;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (soundTimer > 0) soundTimer -= Time.deltaTime;
+        if (soundTimer < 0) soundTimer = -1;
+        
+        float speed;
         if (Vector2.Distance(transform.position, target.position) <= 3 && Vector2.Distance(transform.position, target.position) > 1)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speedFast * Time.deltaTime);
+            speed = speedFast;
         }
-
         else if(Vector2.Distance(transform.position, target.position) >= 4)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speedSlow * Time.deltaTime);
+            speed = speedSlow;
         }
-
+        else
+        {
+            return;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (soundTimer < 0 && other.gameObject.CompareTag("Player"))
         {
             giggle.Play();
+            soundTimer = soundLength;
         }
     }
 }
