df3f26d279c5f87842b3a39d132d7c4af73bacea
bec <rebecca.anne.kerr@gmail.com>
2021-01-30 12:52:25 +0800

    Starting to add title screens (before crashy thing)

---
+++ Assets/Scripts/LoadSceneOne.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneOne : MonoBehaviour
+{
+    public Image fadeblack;
+    public Animator fadeanim;
+    public int level;
+   
+    // Start is called before the first frame update
+   public void loadScene()
+    {
+        StartCoroutine(Fading());
+    }
+
+    IEnumerator Fading()
+    {
+        fadeanim.SetBool("Start", true);
+        yield return new WaitUntil(() => fadeblack.color.a == 1);
+        LoadScene(level);
+    }
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
