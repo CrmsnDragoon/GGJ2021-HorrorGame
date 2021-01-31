9bbaf9ea933871b0c81656fa59520bb9437a74b0
Dylan Sinnott <crmsndragoon@gmail.com>
2021-01-31 16:37:24 +0800

    Just need to set up to the UI

--- Assets/QuestionTime.cs
+++ Assets/QuestionTime.cs
@@ -1,23 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class QuestionTime : MonoBehaviour
 {
+    [SerializeField] private GameObject QuestionUI;
     [SerializeField] private string[] Correct = new string[1] {"Correct."};
     [SerializeField] private string[] IncorrectDia = new string[1] {"Incorrect. Try again."};
     public void AnswerCorrect()
     {
+        QuestionUI.SetActive(false);
         DialogueSystem.Instance.OnDialogueEndEvent.AddListener(CorrectEndOfDialogue);
         DialogueSystem.Instance.ShowDialogue(Correct, null);
     }
     public void Incorrect()
     {
+        QuestionUI.SetActive(false);
         DialogueSystem.Instance.ShowDialogue(IncorrectDia, null);
     }
 
     private void CorrectEndOfDialogue()
     {
         this.transform.parent.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        Global.BlockInput();
+        QuestionUI.SetActive(true);
     }
 }
