1afb4e3ec15174c34d81a035d92e70ec4c1a6635
Dylan Sinnott <crmsndragoon@gmail.com>
2021-01-31 17:19:55 +0800

    Trinkets state reflected in the journal

---
+++ Assets/CollectableTrinket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CollectableTrinket : MonoBehaviour
+{
+    [SerializeField] private ItemType item;
+    [SerializeField] private UnityEngine.UI.Image image;
+    // Start is called before the first frame update
+    void Start()
+    {
+        image.enabled = (false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Global.TrinketIsCollected(item))
+        {
+            image.enabled = (true);
+            this.enabled = (false);
+        }
+    }
+}
