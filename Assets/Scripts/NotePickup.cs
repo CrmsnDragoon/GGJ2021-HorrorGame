8a5de8af3f5c3f5b35a0338ea542a726c58ad8fb
bec <rebecca.anne.kerr@gmail.com>
2021-01-31 13:17:36 +0800

    Crumpled paper added

--- Assets/Scripts/NotePickup.cs
+++ Assets/Scripts/NotePickup.cs
@@ -6,12 +6,21 @@ public class NotePickup : MonoBehaviour
     [SerializeField] private int NoteIndex;
 
     [SerializeField] bool isSimonsNote;
-    
+
+    public AudioSource pickUp;
+
+    private void Start()
+    {
+        GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.gameObject.CompareTag("Player")) return;
+        pickUp.Play();
         this.gameObject.SetActive(false);
         Notes.Instance.ShowNote(NoteIndex);
         Global.HasSimonsNote = isSimonsNote;
+        pickUp.Play();
     }
 }
