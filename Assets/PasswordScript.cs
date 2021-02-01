bab74bdf1310f4782252c03c33d408f39bb0b543
bec <rebecca.anne.kerr@gmail.com>
2021-02-01 13:00:29 +0800

    Added password dialogue from lvl 2 to lvl 3

---
+++ Assets/PasswordScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasswordScript : MonoBehaviour
+{
+    public GameObject passwordSimon;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+
+        {
+            passwordSimon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+
+        {
+            passwordSimon.SetActive(false);
+        }
+    }
+}
